af39967db6108e766ac7c066ff0de5c78ecca6b0
Champux <alvaroa00139@gmail.com>
2026-05-03 12:18:27 +0300

    first commit

---
+++ src/main/java/com/alvaro/pagoya/PagoyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.alvaro.pagoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoyaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagoyaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alvaro/pagoya/PagoyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaro.pagoya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoyaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
